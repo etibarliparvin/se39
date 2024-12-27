784bc92d74fae727b1732a79ecee935654d6e3a2
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-27 21:11:04 +0400

    lesson 4

--- src/main/java/az/ingress/Main.java
+++ src/main/java/az/ingress/Main.java
@@ -2,6 +2,60 @@ package az.ingress;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // type   variable value
+//        boolean b = true; // boolean -> type, b -> variable, true -> deyer, value // 1 bit
+//        byte k = 1; // byte -> type; k -> variable, 1 deyer
+//        byte t = 127; // byte 8 bit
+//        short s = 655; // 2 byte 16 bit
+//        int i = 2147483647;
+//        long l = 153;
+//        long ll = 123L;
+//        long abc = 1234l;
+//        float f = 5.5F;
+//        float ff = 5.5f;
+//        double d = 6.6;
+//        char c = '6';
+        // -128 - +127
+//        byte a = 70;
+//        byte b = 60;
+//        byte c = a + b;
+//        int a = 5;
+//        long b = 7;
+//        long c = a + b;
+//        byte a = 60;
+//        byte b = 70;
+//        byte c = (byte) (a + b);
+//        System.out.println(c);
+//        int a = 2147483647;
+//        int b = 1;
+//        int c = a + b;
+//        System.out.println(c);
+//        short a = 5;
+//        short b = 6;
+//        short c = (short) (a + b);
+//        long a = 2147483647;
+//        int b = 1;
+//        int c = a + b;
+//        System.out.println(c);
+//        long l = 21474836478L;
+//        int i = (int) l;
+//        System.out.println(l);
+//        System.out.println(i);
+//        double d = 5.123456789123456D;
+//        float f = 5.123456789123456F;
+//        System.out.println(d);
+//        System.out.println(f);
+//        char abc = 'a';
+//        char klm = 'b';
+//        char x = 97;
+//        char y = 98;
+//        char z = (char) (x + y);
+//        System.out.println(abc + klm);
+//        System.out.println(x + y);
+//        System.out.println("" + abc + klm);
+        char x = '\uD83D';
+        System.out.println(x);
+        String emoji = "\uD83D\uDE04"; // Grinning Face with Smiling Eyes (😁)
+        System.out.println("Emoji: " + emoji);
     }
 }
