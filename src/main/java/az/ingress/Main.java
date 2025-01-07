3a3dc095b27539749fb5de92d7716ef089c2a7e3
Parvin Etibarli <etibarliparvin@gmail.com>
2025-01-07 21:01:42 +0400

    lesson5-operations

--- src/main/java/az/ingress/Main.java
+++ src/main/java/az/ingress/Main.java
@@ -1,7 +1,86 @@
 package az.ingress;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        char a = 'l'; // 97
+//        char b = 'x'; // 98
+//        System.out.println(a + b); // 195
+//        System.out.println('x' + 'y'); // 195
+//        System.out.println("salam: " + a + b); // 97, 98
+//        System.out.println("sagol: " + 'a' + 'b'); // ab
+//        System.out.println('a' + 'b' + " hello");
+//        float f = 0.6F;
+//        System.out.println(f + 0.1F);
+//
+//        double result = 0;
+//        for (int i = 0; i < 10; i++) {
+//            result += 0.1D;
+//        }
+//        System.out.println(result);
+//
+//        BigDecimal result = new BigDecimal("0");
+//        BigDecimal f = new BigDecimal("0.1");
+//        for(int i = 0; i < 10; i++) {
+//            result = result.add(f);
+//        }
+//        System.out.println(result);
+//
+//        int a = 5;
+//        int b = 2;
+//        System.out.println( a / (float) b);
+//        int a = 7;
+//        int b = 3;
+//        System.out.println(a % b);
+//
+//        double a = 5.1;
+//        double b = 2.5;
+//        System.out.println(a % b);
+//        System.out.println("Please insert any number:");
+//        Scanner abc = new Scanner(System.in);
+//        int a = abc.nextInt();
+//        if(a % 2 == 0) {
+//            System.out.println("cut");
+//        } else {
+//            System.out.println("tek");
+//        }
+//        System.out.println("Please insert amount:");
+//        Scanner sc = new Scanner(System.in);
+//        double amount = sc.nextDouble();
+//        if(amount % 1 == 0) {
+//            System.out.println("integer number");
+//        } else {
+//            System.out.println("floating point number");
+//        }
+//        int a = 5;
+//        a %= 3;
+//        a = a % 3;
+//        System.out.println(a);
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        boolean result = (a != b);
+//        System.out.println(result);
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt(); // 1
+//        int b = sc.nextInt(); // 10
+//        int c = sc.nextInt(); // 2
+//        int d = sc.nextInt(); // 19
+//        int sum1 = a + b;
+//        int sum2 = c + d;
+//        boolean result = (sum1 == sum2) || (sum1 > 10);
+//        System.out.println(result);
+        Scanner ab = new Scanner(System.in);
+        int age = ab.nextInt();
+        int salary = ab.nextInt();
+        boolean isPhysicalPerson = ab.nextBoolean();
+        boolean isRetiree = ab.nextBoolean();
+
+        if((age > 18 && age < 65) && isPhysicalPerson &&) {
+            System.out.println("You may be given 30_000 manat credit");
+        } else if(age > 65 || )
     }
+
 }
